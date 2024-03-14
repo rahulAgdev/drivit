1a6d4358c3e464bf5241b71f6e24cea70cdee815
Rahul Agarwal <rahulagdev@gmail.com>
2024-03-14 09:29:59 +0530

    Trying query and mutations in convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import {
   SignInButton,
   SignOutButton,
@@ -7,19 +8,33 @@ import {
   SignedOut,
   useSession,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
   const session = useSession();
   return (
     <div className=" flex gap-5">
       <SignedIn>
-        <SignOutButton />
+        <SignOutButton>
+          <Button>Sign out</Button>
+        </SignOutButton>
       </SignedIn>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
       </SignedOut>
-      <Button>Button</Button>
+      {/* this button will call the create file, which will invoke the mutation in the files.ts in convex to then create a file in the backend.  */}
+      <Button onClick={()=>{createFile({
+        name:"Hello world"
+      })}}>Click me</Button>
+      {/* now using a query, we can fetch this data, and then display it in the frontend */}
+      {files?.map((file)=>{
+        return <div key={file._id}>{file.name}</div>
+      })}
     </div>
   );
 }
---
+++ convex/files.ts
@@ -0,0 +1,24 @@
+import { v } from 'convex/values'
+import {mutation, query} from './_generated/server'
+
+// mutation is a function that will call the convex server and call in mutation functions, query functions, or actions ( 3rd party connections)
+export const createFile = mutation({
+    args: {
+        name: v.string(),
+    },
+    async handler(ctx, args){
+        // from the frontend we can pass and compute some info
+        // assume that we are getting a name from a form, and it is of the form string 
+        await ctx.db.insert('files', {
+            name: args.name,
+            // ctx is context, now we can invoke this function from the frontend, and store or update files. 'files' here can be a table name.
+        })
+    }
+})
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx, args){
+        return ctx.db.query('files').collect();
+    }
+})
